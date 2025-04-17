bba4a0c21df5c7910d16fa1a9b9ab23f6db13bb0
divishtk <divisht.avion@hotmail.com>
2025-04-17 20:23:23 +0530

    feat(auth): implement password matching validation in MatchPassword class

--- src/app/auth/validators/match-password.ts
+++ src/app/auth/validators/match-password.ts
@@ -1,2 +1,19 @@
-export class MatchPassword {
+import {
+  AbstractControl,
+  FormGroup,
+  ValidationErrors,
+  Validator,
+} from '@angular/forms';
+
+export class MatchPassword implements Validator {
+  validate(formGroup: FormGroup) {
+    const { password, passwordConfirmation } = formGroup.value;
+    if (password === passwordConfirmation) {
+      return null;
+    } else {
+      return {
+        passwordNotMatched: true,
+      };
+    }
+  }
 }
