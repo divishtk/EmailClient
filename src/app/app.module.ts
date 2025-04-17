240abcf89595649aa2e36c6b8a158fb0ed0620a9
divishtk <divisht.avion@hotmail.com>
2025-04-17 20:09:51 +0530

    feat(auth): implement sign-up form with validation and integrate Semantic UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthModule } from './auth/auth.module';
-import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -15,7 +14,6 @@ import { ReactiveFormsModule } from '@angular/forms';
     BrowserModule,
     AppRoutingModule,
     AuthModule,
-    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/auth/sign-up/sign-up.component.ts
+++ src/app/auth/sign-up/sign-up.component.ts
@@ -1,20 +1,30 @@
 import { Component } from '@angular/core';
-import { FormControl ,FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
   standalone: false,
   templateUrl: './sign-up.component.html',
-  styleUrl: './sign-up.component.css'
+  styleUrl: './sign-up.component.css',
 })
 export class SignUpComponent {
-
-
   signUpForm = new FormGroup({
-    username: new FormControl(''),
-   // email: new FormControl(''),
-    password: new FormControl(''),
-    confirmPassword: new FormControl(''),
-  })
-
+    username: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(20),
+      Validators.pattern(/^[a-z0-9]+$/),
+    ]),
+    // email: new FormControl(''),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(4),
+      Validators.maxLength(20),
+    ]),
+    passwordConfirmation: new FormControl('',[
+      Validators.required,
+      Validators.minLength(4),
+      Validators.maxLength(20),
+    ]),
+  });
 }
