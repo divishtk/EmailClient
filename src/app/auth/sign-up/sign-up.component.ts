aa6fac1e75b30c2d6b82bf46ff8452c992532d40
divishtk <divisht.avion@hotmail.com>
2025-04-17 23:09:24 +0530

    feat(auth): enhance sign-up form with password confirmation validation

--- src/app/auth/sign-up/sign-up.component.ts
+++ src/app/auth/sign-up/sign-up.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatchPassword } from '../validators/match-password';
 
 @Component({
   selector: 'app-sign-up',
@@ -7,24 +8,36 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.css',
 })
-export class SignUpComponent {
-  signUpForm = new FormGroup({
-    username: new FormControl('', [
-      Validators.required,
-      Validators.minLength(3),
-      Validators.maxLength(20),
-      Validators.pattern(/^[a-z0-9]+$/),
-    ]),
-    // email: new FormControl(''),
-    password: new FormControl('', [
-      Validators.required,
-      Validators.minLength(4),
-      Validators.maxLength(20),
-    ]),
-    passwordConfirmation: new FormControl('',[
-      Validators.required,
-      Validators.minLength(4),
-      Validators.maxLength(20),
-    ]),
-  });
+export class SignUpComponent implements OnInit {
+  constructor(private matchPassword: MatchPassword) {
+    console.log("mathc pass", this.matchPassword);
+  }
+
+  signUpForm!: FormGroup;
+
+  ngOnInit() {
+    this.signUpForm = new FormGroup(
+      {
+        username: new FormControl('', [
+          Validators.required,
+          Validators.minLength(3),
+          Validators.maxLength(20),
+          Validators.pattern(/^[a-z0-9]+$/),
+        ]),
+        password: new FormControl('', [
+          Validators.required,
+          Validators.minLength(4),
+          Validators.maxLength(20),
+        ]),
+        passwordConfirmation: new FormControl('', [
+          Validators.required,
+          Validators.minLength(4),
+          Validators.maxLength(20),
+        ]),
+      },
+      {
+        validators: [this.matchPassword.validate.bind(this.matchPassword)]
+      }
+    );
+  }
 }
--- src/app/auth/validators/match-password.ts
+++ src/app/auth/validators/match-password.ts
@@ -1,13 +1,18 @@
+import { Injectable } from '@angular/core';
 import {
   AbstractControl,
   FormGroup,
   ValidationErrors,
   Validator,
+  ValidatorFn,
 } from '@angular/forms';
 
+@Injectable({ providedIn: 'root' })
 export class MatchPassword implements Validator {
-  validate(formGroup: FormGroup) {
+  /*validate(formGroup: FormGroup) {
     const { password, passwordConfirmation } = formGroup.value;
+    console.log("1")
+    console.log(password ,'',passwordConfirmation)
     if (password === passwordConfirmation) {
       return null;
     } else {
@@ -15,5 +20,17 @@ export class MatchPassword implements Validator {
         passwordNotMatched: true,
       };
     }
-  }
+  }*/
+
+    validate: ValidatorFn = (control: AbstractControl): ValidationErrors | null => {
+        const group = control as FormGroup;  
+        
+        const { password, passwordConfirmation } = group.value;  
+        
+        if (password === passwordConfirmation) {
+          return null;  
+        } else {
+          return { passwordNotMatched: true };  
+        }
+      };
 }
