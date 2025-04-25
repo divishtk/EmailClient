a20fbadf3712bd30d19165a2c4305a790c5bd4e7
divishtk <divisht.avion@hotmail.com>
2025-04-25 19:08:32 +0530

    feat(inbox): add Notfound component and routing; enhance email resolver for error handling; update email display styles

--- src/app/inbox/email-resolver.service.ts
+++ src/app/inbox/email-resolver.service.ts
@@ -1,17 +1,29 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from '@angular/router';
+import {
+  ActivatedRouteSnapshot,
+  Resolve,
+  Router,
+  RouterStateSnapshot,
+} from '@angular/router';
 import { Email } from './email';
 import { EmailService } from './email.service';
+import { catchError, EMPTY } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class EmailResolverService implements Resolve<Email> {
-
-  constructor(private emailService: EmailService) {
-  }
+  constructor(private emailService: EmailService, private router: Router) {}
   resolve(route: ActivatedRouteSnapshot) {
     const { id } = route.params;
-    return this.emailService.fetchEmailById(id);
+    // if(!id){
+    //     this.router.navigateByUrl('/inbox/not-found');
+    // }
+    return this.emailService.fetchEmailById(id).pipe(
+      catchError(() => {
+        this.router.navigateByUrl('/inbox/not-found');
+        return EMPTY;
+      })
+    );
   }
 }
--- src/app/inbox/inbox-routing.module.ts
+++ src/app/inbox/inbox-routing.module.ts
@@ -4,12 +4,17 @@ import { InboxHomeComponent } from './inbox-home/inbox-home.component';
 import { PlaceholderComponent } from './placeholder/placeholder.component';
 import { EmailShowComponent } from './email-show/email-show.component';
 import { EmailResolverService } from './email-resolver.service';
+import { NotfoundComponent } from './notfound/notfound.component';
 
 const routes: Routes = [
   {
     path: '',
     component:InboxHomeComponent,
     children:[
+      {
+        path:'not-found',
+        component:NotfoundComponent
+      },
       {
         path:'',
         component:PlaceholderComponent
